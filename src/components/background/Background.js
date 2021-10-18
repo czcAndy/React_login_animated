0799a8c2567ee83a220504362ae27fd705d13806
Andrei Cazaceanu <andrei.cazaceanu@gmail.com>
2021-10-18 19:51:14 +0200

    Revert all changes - back to step 1 :)

--- src/components/background/Background.js
+++ src/components/background/Background.js
@@ -1,39 +1,5 @@
-import styles from "./Background.module.css";
-import variants from "../../ui/animations/variants";
-import { MotionRectangle, Rectangle } from "../../ui/designElements/rectangle/Rectangle";
-import { motion } from "framer-motion";
-
-const GRADIENT_COLORS = "#3f37c9ff, #4361eeff";
-
 const Background = (props) => {
-  return (
-    <div className={styles.background}>
-      <MotionRectangle
-        height={"30vh"}
-        colors={GRADIENT_COLORS}
-        variants={variants}
-        initial="initial_right"
-        animate="animate_horizontally"
-      ></MotionRectangle>
-       <MotionRectangle height={"40vh"} colors={"white, white"}></MotionRectangle>
-      <MotionRectangle
-        height={"30vh"}
-        colors={GRADIENT_COLORS}
-        variants={variants}
-        initial="initial_left"
-        animate="animate_horizontally"
-      ></MotionRectangle>
-      <motion.div
-        className={styles["center-content"]}
-        variants={variants}
-        initial="initial_top"
-        animate="animate_vertically_after_sibling"
-      >
-        <MotionRectangle colors={"white, white"} height={"50%"} />
-        <MotionRectangle colors={GRADIENT_COLORS} height={"50%"} />
-      </motion.div>
-    </div>
-  );
+    return <div></div>
 };
 
 export default Background;
--- src/ui/designElements/gradient/Gradient.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-const Gradient = styled.div`
-    background: linear-gradient(${props => props.colors});
-    height: 100%;
-    width: 100%;
-`;
-
-export default Gradient;
